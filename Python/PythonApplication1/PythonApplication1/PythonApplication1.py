06d5f0e172003bf0ce64cfe241acf0fd40e5b605
AlesKas <ales.kasparek01@upol.cz>
2019-05-12 17:12:50 +0200

    Update

--- Python/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ Python/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -90,3 +90,70 @@ while True:
         break
 if count:
     print("pocet =", count, "celkem =", total, "max =", max, "min =", min, "cisla:", numbers)
+
+def get_int(msg, min, default):
+    while True:
+        try:
+            line = input(msg)
+            if not line and default is not None:
+                return default
+            i = int(line)
+            if i < min:
+                print("musi byt >=", min)
+            else:
+                return i
+        except ValueError as err:
+            printf(err)
+
+#rows = get_int("rows: ", 1, None)
+#col = get_int("columns: ", 1, None)
+#min = get_int("min: ", -10000000, 0)
+#default = 1000
+#if default < min:
+#    default = 2 * min
+#max = get_int("maximum:", min, default)
+
+#row = 0
+#while row < rows:
+#    line = ""
+#    column = 0
+#    while column < col:
+#        i = random.randint(min, max)
+#        s = str(i)
+#        while len(s) < 10:
+#            s = " " + s
+#        line += s
+#        column += 1
+#    print(line)
+#    row += 1
+
+print("-------------------")
+
+zajmena = ["muj", "tvuj", "jeji", "jeho"]
+pods = ["kočka", "pes", "kůň", "muž", "žena", "kluk", "holka"]
+slovesa = ["zpívá", "běží", "skáče", "říka", "bojuje", "plave", "vidí",
+         "slyší", "cítí", "spí", "hopká", "doufá", "pláče",
+         "se směje", "kráčí"]
+prislovce = ["hlasitě", "tiše", "rychle", "pomalu", "kvalitně", "hrozně",
+           "hrubě", "zdvořile"]
+lines = 5
+if len(sys.argv) > 1:
+    try:
+        temp = int(sys.argv[1])
+        if 1 <= temp <= 10:
+            lines = temp
+        else:
+            print("řádků musí být 1-10 včetně")
+    except ValueError:
+        print("použití: badpoetry.py [řádky]")
+
+while lines:
+    article = random.choice(zajmena)
+    subject = random.choice(pods)
+    verb = random.choice(slovesa)
+    if random.randint(0, 1) == 0:
+        print(article, subject, verb)
+    else:
+        adverb = random.choice(prislovce)
+        print(article, subject, adverb, verb)
+    lines -= 1
