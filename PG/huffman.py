f99c4471a346cb84c8af656ef21d292833a11c36
AlesKas <48322498+AlesKas@users.noreply.github.com>
2019-11-20 21:46:50 +0100

    Huffmans code

--- PG/huffman.py
+++ PG/huffman.py
@@ -1,3 +1,6 @@
+import json
+import sys
+
 from nodeTree import NodeTree
 
 def huffmanCodeTree(node, left=True, binString=""):
@@ -5,11 +8,11 @@ def huffmanCodeTree(node, left=True, binString=""):
         return {node: binString}
     (l, r) = node.children()
     d = dict()
-    d.update(huffmanCodeTree(l, True, binString + "0"))
-    d.update(huffmanCodeTree(r, False, binString + "1"))
+    d.update(huffmanCodeTree(l, True, "0"+ binString))
+    d.update(huffmanCodeTree(r, False, "1" + binString))
     return d    
 
-def main(string):
+def encode(string):
     freq = {}
     for c in string:
         if c in freq:
@@ -30,11 +33,47 @@ def main(string):
 
     huffmanCode = huffmanCodeTree(nodes[0][0])
 
-    print(" Char | Freq  | Huffman code ")
-    print("-----------------------------")
-    for char, frequency in freq:
-        print(" %-4r | %5d | %12s" % (char, frequency, huffmanCode[char]))
+
+    for key, val in huffmanCode.items():
+        huffmanCode[key] = val.rjust(8, '0')
+
+
+    with open('table.json', 'w') as fp:
+        res = {v : k for k, v in huffmanCode.items()}
+        json.dump(res, fp)
+
+    with open('output.bin', 'wb') as file:
+        for ch in string:
+            data = huffmanCode[ch]
+            byte = int(data, 2)
+            bit = "{0:b}".format(byte).rjust(8, '0')
+            #print(f"{ch} = {bytes([byte])} = {bit} = {[key for key, val in huffmanCode.items() if bit == val]}")
+            file.write(bytes([byte]))
+
+def decode():
+    with open('output.bin', 'rb') as inputFile, open('table.json', 'r') as file:
+        tableDict = json.load(file)
+        data = inputFile.read(1)
+        res = ""
+        while data:
+            val = int.from_bytes(data, byteorder='big')
+            bit = "{0:b}".format(val).rjust(8, '0')
+            res += tableDict[bit]
+            data = inputFile.read(1)
+    print(res)
+
+    with open('result.txt', 'w') as file:
+        file.write(res)
 
 if __name__ == "__main__":
-    string = input("Enter string to encode: ")
-    main(string)
+    op = input("Enter command, e for encode, d for decode: ")
+    if op is "e":
+        with open('input.txt', 'r') as inputFile:
+            string = inputFile.read()
+            encode(string)
+    elif op is "d":
+        decode()
+
+    else:
+        print("Unsupported operation.")
+        exit(-1)
