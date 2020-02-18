88d48d173968e3e522237b8e4bdedb933d6538a1
AlesKas <48322498+AlesKas@users.noreply.github.com>
2020-02-18 15:50:30 +0100

    class changes

--- OS1 - 2/OS1-2/OS1-2/Source.c
+++ OS1 - 2/OS1-2/OS1-2/Source.c
@@ -2,98 +2,85 @@
 #include <stdio.h>
 #include <math.h>
 
-int obvod_obdelnika(int a, int b)
-{
+int obvod_obdelnika(int a, int b) {
 	_asm {
 		mov eax, a
 		add eax, b
 		mov ebx, 2
-		mul ebx
-	}
-}
-int obsah_obdelnika(int a, int b)
-{
-	_asm
-	{
-		mov eax, b
-		MUL a
+		shl eax, 1
 	}
 }
 
-int obvod_ctverce(int a)
-{
+int obsah_obdelnika(int a, int b) {
 	_asm {
 		mov eax, a
-		SHL eax, 2
+		mul b
+	}
+}
+
+int obvod_ctverce(int a) {
+	_asm {
+		mov eax, a
+		shl eax, 2
 	}
 }
 
 int obsah_ctverce(int a) {
-	_asm
-	{
+	_asm {
 		mov eax, a
-		MUL eax
-	}
-}
-int obvod_trojuhelnika(int a, int b, int c)
-{
-	_asm
-	{
-		mov eax, a
-		add eax, b
-		add eax, c
+		mul a
 	}
 }
 
-int obvod_trojuhelnika2(int a)
-{
-	_asm
-	{
-		mov eax, a
-		add eax, a
-		add eax, a
-	}
-}
-
-int obsah_trojuhelnika2(int a, int b)
-{
-	_asm
-	{
-		mov eax, a
-		MUL b
-		shr eax, 1
-	}
-}
-
-int obsah_trojuhelnika3(int a, int va)
-{
-	_asm
-	{
-		mov eax, a
-		MUL va
-		shr eax, 1
-	}
-}
-
-int objem_krychle(int a)
-{
-	_asm
-	{
-		mov eax, a
-		MUL a
-		MUL a
-	}
-}
-
-double heronuv_vzorec(int a, int b, int c)
-{
-	int result;
+int obvod_trojuhelnika(int a, int b, int c) {
 	_asm {
 		mov eax, a
 		add eax, b
 		add eax, c
+	}
+}
+
+int obvod_trojuhelnika2(int a) {
+	_asm {
+		mov eax, a
+		mov ebx, 3
+		mul ebx
+	}
+}
+
+int obsah_trojuhelnika2(int a, int b) {
+	_asm {
+		mov eax, a
+		mul b
 		shr eax, 1
-		mov esi, eax
+	}
+}
+
+int obsah_trojuhelnika3(int a, int va) {
+	_asm {
+		mov eax, a
+		mul va
+		shr eax, 1
+	}
+}
+
+int objem_krychle(int a) {
+	_asm {
+		mov eax, a
+		mul a
+		mul a
+	}
+}
+
+double heronuv_vzorec(int a, int b, int c) {
+	int result;
+	_asm {
+		mov esi, a
+		add esi, b
+		add esi, c
+		shr esi, 1
+
+		mov eax, esi
 
 		mov ebx, esi
 		sub ebx, a
@@ -121,7 +108,7 @@ int main()
 	printf("obvod_trojuhelnika %d\n", obvod_trojuhelnika(5, 6, 7));
 	printf("obvod_trojuhelnika2 %d\n", obvod_trojuhelnika2(5));
 	printf("obsah_trojuhelnika2 %d\n", obsah_trojuhelnika2(5, 6));
-	printf("obsah_trojuhelnika3 %d\n", obsah_trojuhelnika3(5, 7));
+	printf("obsah_trojuhelnika3 %d\n", obsah_trojuhelnika3(5, 6));
 	printf("objem_krychle %d\n", objem_krychle(5));
 	printf("heronuv_vzorec %f\n", heronuv_vzorec(5, 6, 7));
 
