20a51a6ba2f4bb790baec739ff827615f913c84b
AlesKas <48322498+AlesKas@users.noreply.github.com>
2020-05-08 15:03:05 +0200

    WTF

---
+++ OS1 - 2/Zápočet/Zápočet-ASM/Zápočet-ASM.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+    return 0;
+}
---
+++ OS1 - 2/Zápočet/canvas/canvas.c
@@ -0,0 +1,241 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+
+//Informace o bodu jsou uloženy ve dvou bitech
+//První bit odpovídá informaci o horizontální čáře - 0 nepřítomna, 1 přítomna
+//Druhý bit odpovídá informaci o vertikální čáře - 0 nepřítomna, 1 přítomna
+
+struct canvas {
+	char* mask;
+	int width;
+	int height;
+	int size;
+};
+
+//Pomocná funkce pro rotaci bitů doprava
+unsigned int rotr(char num, unsigned int rotation)
+{
+	rotation %= 7;
+
+	while (rotation--)
+		num = ((num >> 1)& (~(1 << 7)) | ((num & 1) << 7));
+
+	return num;
+}
+
+struct canvas* canvas_create(int width, int height);
+void canvas_init(struct canvas* canvas);
+void canvas_free(struct canvas* canvas);
+void canvas_print(struct canvas* canvas);
+void canvas_hline(struct canvas* canvas, unsigned int x, unsigned int y, int length);
+void canvas_vline(struct canvas* canvas, unsigned int x, unsigned int y, int length);
+
+int main() {
+	struct canvas* c = canvas_create(20, 10);
+	canvas_hline(c, 3, 2, 13);
+	canvas_hline(c, 14, 5, -11);
+	canvas_vline(c, 4, 1, 6);
+	canvas_vline(c, 14, 6, -6);
+	canvas_vline(c, 9, 5, 20);
+	canvas_print(c);
+	canvas_free(c);
+	return 0;
+}
+
+struct canvas* canvas_create(int width, int height){
+	int size = (width * height) / 8;
+	size *= 2;
+	struct canvas* temp = malloc(sizeof(struct canvas));
+	temp->mask = malloc(size);
+	temp->height = height;
+	temp->width = width;
+	temp->size = size;
+	canvas_init(temp);
+	return temp;
+}
+
+//Inicializace plátna, na začátku je plátno bílé
+void canvas_init(struct canvas* canvas) {
+	for (int i = 0; i < canvas->size; i++) {
+		canvas->mask[i] = 0b00000000;
+	}
+}
+
+//Funcke pro uvolnění plátna z paměti
+void canvas_free(struct canvas* canvas) {
+	free(canvas->mask);
+	free(canvas->width);
+	free(canvas->height);
+	free(canvas->size);
+	free(canvas);
+}
+
+//Funce pro tisk plátna, prochází se jednotlivé dvoubity, a zjišťuje se, je-li
+//přítomná svislá nebo vodorovná čára
+void canvas_print(struct canvas* canvas) {
+	int linesWritten = 0;
+	char mask = 0b11000000;
+	for (int i = 0; i < canvas->size; i++) {
+		char item = canvas->mask[i];
+		for (int j = 0; j < 8; j += 2) {
+			int location = item & mask;
+			item = item << 2;
+
+			if (!location)			//0b00000000
+				printf(".");
+			else if (location == -64)   //0b11000000
+				printf("+");
+			else if (location == -128)  //0b10000000
+				printf("-");
+			else                        //0b01000000
+				printf("|");
+
+			linesWritten++;
+			if (linesWritten == canvas->width)
+			{
+				printf("\n");
+				linesWritten = 0;
+			}
+		}
+	}
+}
+
+void canvas_hline(struct canvas* canvas, unsigned int x, unsigned int y, int length) {
+	int bitsOnLine = canvas->width * 2;
+	int preBits = 1;
+	int postBits = 1;
+
+	if (length > 0) {
+		int startBit = y * bitsOnLine;
+		startBit += (2 * x);
+		int endBit = startBit + (length * 2);
+		int bitsManipulated = 0;
+		int offset = 0;
+		for (int i = 0; i < canvas->size; i++) {
+			int localStartBit = i * 8;
+			int localEndBit = i * 8 + 7;
+			if ((i + 1) * bitsOnLine > startBit && !offset)
+				offset = startBit - (i * bitsOnLine);
+			if (localStartBit <= startBit && startBit <= localEndBit && preBits) {
+				int bitsToRotate = startBit - localStartBit;
+				for (int j = bitsToRotate; j < 8; j += 2) {
+					canvas->mask[i] = canvas->mask[i] << 2;
+					canvas->mask[i] |= 0b00000010;
+					bitsManipulated += 2;
+				}
+				preBits = 0;
+			}
+			if (startBit <= i * 8 && endBit > i * 8 + 7) {
+				canvas->mask[i] |= 0b10101010;
+				bitsManipulated += 8;
+			}
+			if (localStartBit <= endBit && endBit <= localEndBit && postBits) {
+				int bitsToRotate = endBit - localStartBit;
+				for (int j = 8 - bitsToRotate; j < 8; j += 2) {
+					canvas->mask[i] |= 0b00000010;
+					canvas->mask[i] = rotr(canvas->mask[i], 2);
+					bitsManipulated += 2;
+				}
+				postBits = 0;
+			}
+			if (bitsManipulated >= (bitsOnLine - offset))
+				return;
+		}
+	}
+	else if (length < 0) {
+		int startBit = y * bitsOnLine;
+		startBit += (2 * x + 2);
+		int endBit = startBit;
+
+		int col = endBit / bitsOnLine;
+		int colStartBit = col * bitsOnLine;
+		startBit += (2 * length + 2);
+
+		if (startBit < colStartBit)
+			startBit = colStartBit;
+
+		for (int i = 0; i < canvas->size; i++) {
+			int localStartBit = i * 8;
+			int localEndBit = i * 8 + 7;
+			if (localStartBit <= startBit && startBit <= localEndBit && preBits) {
+				int bitsToRotate = startBit - localStartBit;
+				canvas->mask[i] = rotr(canvas->mask[i], bitsToRotate);
+				for (int j = bitsToRotate; j <= 8; j+=2) {
+					canvas->mask[i] |= 0b00000010;
+					canvas->mask[i] = rotr(canvas->mask[i], 2);
+				}
+				preBits = 0;
+			}
+			if (startBit <= i * 8 && endBit > i * 8 + 7) {
+				canvas->mask[i] |= 0b10101010;
+			}
+			if (localStartBit <= endBit && endBit <= localEndBit && postBits) {
+				int bitsToRotate = endBit - localStartBit;
+				for (int j = 8 - bitsToRotate; j < 8; j += 2) {
+					canvas->mask[i] |= 0b00000010;
+					canvas->mask[i] = rotr(canvas->mask[i], 2);
+				}
+				postBits = 0;
+			}
+		}		
+	}
+}
+
+void canvas_vline(struct canvas* canvas, unsigned int x, unsigned int y, int length) {
+	int bitsToNextLine = canvas->width * 2;
+	if (length > 0) {
+		int startBit = y * bitsToNextLine;
+		startBit += 2 * x + 2;
+		int endBit = startBit + length * bitsToNextLine;
+		int nextBitToRotate = startBit;
+		int rotationFlag = 1;
+		int postBits = 1;
+		int rotation = 0;
+		for (int i = 0; i < canvas->size; i++) {
+			int localStartBit = i * 8;
+			int localEndBit = i * 8 + 7;
+			if (nextBitToRotate <= localEndBit && localEndBit <= endBit) {
+				if (rotationFlag) {
+					rotation = localStartBit - startBit;
+					rotationFlag = 0;
+				}
+				if (nextBitToRotate > canvas->width* canvas->height * 2) {
+					return;
+				}
+				canvas->mask[i] = rotr(canvas->mask[i], rotation);
+				canvas->mask[i] |= 0b00000001;
+				canvas->mask[i] = rotr(canvas->mask[i], 8 - rotation - 1);
+				nextBitToRotate += bitsToNextLine;
+			}
+		}
+	}
+	else if (length < 0) {
+		int endBit = y * bitsToNextLine;
+		endBit += 2 * x + 2;
+		int startBit = endBit + (length + 1) * bitsToNextLine;
+		int nextBitToRotate = startBit;
+		int rotationFlag = 1;
+		int postBits = 1;
+		int rotation = 0;
+		for (int i = 0; i < canvas->size; i++) {
+			int localStartBit = i * 8;
+			int localEndBit = i * 8 + 7;
+			if (nextBitToRotate <= localEndBit && nextBitToRotate <= endBit) {
+				if (rotationFlag) {
+					rotation = startBit - localStartBit;
+					rotationFlag = 0;
+				}
+				if (nextBitToRotate > canvas->width* canvas->height * 2) {
+					return;
+				}
+				canvas->mask[i] = rotr(canvas->mask[i], 8 - rotation);
+				canvas->mask[i] |= 0b00000001;
+				canvas->mask[i] = rotr(canvas->mask[i], rotation);
+				nextBitToRotate += bitsToNextLine;
+			}
+		}
+	}
+}
